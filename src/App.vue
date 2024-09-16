297ed0cdca63b627bcb94e47dda4a7b1bb9aa4f9
MD. SHOHAG HOSSAIN <mdshohaghossain8080@gmail.com>
2024-09-16 15:47:29 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'Vue v3',
+      status: true,
+      languages: ['HTML', 'css', 'js', 'php', 'vue'],
+      link: 'https://www.google.com'
+    };
+  },
+  methods: {
+    change_status() {
+      if (this.status == true) {
+        this.status = false;
+      } else if (this.status == false) {
+        this.status = true;
+      } else {
+        this.status = true
+      }
+    }
+  }
+};
+</script>
+
+<template>
+  <!-- just view -->
+  <h1>Hi! I'm {{ name }}.</h1>
+
+  <!-- conditonal -->
+  <p v-if="status">User is Active!</p>
+  <p v-else>User is Inactive!</p>
+  <hr />
+
+  <!-- loop -->
+  <h3>My Languages:-</h3>
+  <ol>
+    <li v-for="item in languages" :key="item">{{ item }}</li>
+  </ol>
+
+  <!-- link -->
+  <!-- <a v-bind:href="link">Google</a> -->
+  <a :href="link">Google</a>
+  <br/>
+
+  <!-- on click -->
+  <!-- <button v-on:click="change_status">Change Status</button> -->
+  <button @click="change_status">Change Status</button>
+
+</template>
+
+<style scoped>
+/*  */
+</style>
